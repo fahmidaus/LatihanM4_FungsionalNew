377b844990a87b89df46e74e0239507255da1c4c
fahmidaus <129297258+fahmidaus@users.noreply.github.com>
2023-11-16 12:40:17 +0700

    Add files via upload

---
+++ l6.py
@@ -0,0 +1,16 @@
+def point(x, y):
+    return x, y
+
+def line_equation_of(p1, M):
+    # Inner function untuk menghitung nilai C
+    def hitung_C(x, y):
+        return y - M * x
+
+    # Menggunakan closure untuk mendapatkan nilai C
+    C = hitung_C(*p1)
+
+    return f"y = {M:.2f}x + {C:.2f}"
+
+# Menampilkan persamaan garis yang melalui titik (0, 8) dan bergradien 4
+print("Persamaan garis yang melalui titik (0, 8) dan bergradien 4:")
+print(line_equation_of(point(0, 8), 4))
